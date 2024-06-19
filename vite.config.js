fb799037f0cb3ddba6fa14354352b0fc49c9dc0b
Rahul Bachheti <rahulbachheti2162002@gmail.comm>
2024-06-20 00:29:17 +0530

    updated the base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SBDA_LAB/',  // Set this to your GitHub repository name
+  base: '/SBDA-LAB/',  
 });
